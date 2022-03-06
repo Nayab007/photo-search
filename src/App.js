871a79e4ddb21e21bd901c23d5550d14e46070eb
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-03-06 22:24:09 +0100

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchPhotos from "./searchPhotos"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <div className="container">
+        <h1 className="title">Photo Search</h1>
+        <SearchPhotos />
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/searchPhotos.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Unsplash, { toJson } from 'unsplash-js';
+
+
+const unsplash = new Unsplash({ 
+  accessKey: "EovY5lPZTgt4e6B46YyR34jJT3bntpJBS6HPkSiqvW4",
+ });
+
+
+export default function SearchPhotos() {
+  const [query, setQuery] = useState("");
+    const [pics, setPics] = useState([]);
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        unsplash.search
+    .photos(query)
+    .then(toJson)
+    .then((json) => {
+      setPics(json.results);
+    });
+   };
+
+  return (
+    <>
+         <form className="form" onSubmit={searchPhotos} > 
+        <label className="label" htmlFor="query"> 
+          {" "}
+          📷
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Try "dog" or "apple"`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+      {pics.map((pic) => <div className="card" key={pic.id}>
+      <img
+              className="card--image"
+              alt={pic.alt_description}
+              src={pic.urls.full}
+              width="100%"
+              height="50%"
+            ></img>
+            </div> )};
+      </div>
+    </>
+  );
+}
