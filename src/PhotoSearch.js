b80cb88c9f7017cd0693fad89fe1014a5ecebebf
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-12 07:59:35 +0200

    add

--- src/PhotoSearch.js
+++ src/PhotoSearch.js
@@ -52,7 +52,7 @@ export default function PhotoSearch() {
               alt={pic.alt_description}
               src={pic.urls.full}
               width="100%"
-              height="57%"
+              height="58%"
             ></img>
             </a>
           </div>
