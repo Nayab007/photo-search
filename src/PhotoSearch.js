c0b2ae7523e0b0eea0e20093c2abfa05ef9f0c0a
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-12 07:57:14 +0200

    add

--- src/PhotoSearch.js
+++ src/PhotoSearch.js
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
 
-const API_KEY =`${process.env.REACT_APP_API_KEY}`
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
-const unsplash = new Unsplash({ 
-  accessKey : `${API_KEY}`
- })
-
+const unsplash = new Unsplash({
+  accessKey: `${API_KEY}`,
+});
 
 export default function PhotoSearch() {
-  const [query, setQuery] = useState("");
-    const [pics, setPics] = useState([]);
-    const searchPhotos = async (e) => {
-        e.preventDefault();
-        unsplash.search
-    .photos(query)
-    .then(toJson)
-    .then((json) => {
-      setPics(json.results);
-    });
-   };
+  const [query, setQuery] = useState('');
+  const [pics, setPics] = useState([]);
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+    unsplash.search
+      .photos(query)
+      .then(toJson)
+      .then((json) => {
+        setPics(json.results);
+      });
+  };
 
   return (
     <>
-         <form className="form" onSubmit={searchPhotos} > 
-        <label className="label" htmlFor="query"> 
-          {" "}
+      <form className="form" onSubmit={searchPhotos}>
+        <label className="label" htmlFor="query">
+          {' '}
           📷
         </label>
         <input
@@ -41,15 +40,23 @@ export default function PhotoSearch() {
         </button>
       </form>
       <div className="card-list">
-      {pics.map((pic) => <div className="card" key={pic.id}>
-      <img
+        {pics.map((pic) => (
+          <div className="card" key={pic.id}>
+            <a
+                href={pic.urls.full}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+            <img
               className="card--image"
               alt={pic.alt_description}
               src={pic.urls.full}
               width="100%"
               height="50%"
             ></img>
-            </div> )}
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
